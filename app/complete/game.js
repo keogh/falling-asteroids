8a64918876e6214a96593f24e9b7f80346ee6e34
Isaac Zepeda <keogh24@gmail.com>
2014-11-04 16:57:51 -0700

    Add Entity

--- app/complete/game.js
+++ app/complete/game.js
@@ -182,6 +182,39 @@ GameEngine.prototype.loop = function () {
   this.key = null;
 }
 
+function Entity(game, x, y) {
+  this.game = game;
+  this.x = x;
+  this.y = y;
+  this.removeFromWorld = false;
+}
+
+Entity.prototype.update = function() {
+}
+
+Entity.prototype.draw = function(ctx) {
+  // if (this.game.showOutlines && this.radius) {
+  //   ctx.beginPath();
+  //   ctx.strokeStyle = "green";
+  //   ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+  //   ctx.stroke();
+  //   ctx.closePath();
+  // }
+}
+
+Entity.prototype.drawSpriteCentered = function(ctx) {
+  if (this.sprite && this.x && this.y) {
+    var x = this.x - this.sprite.width/2;
+    var y = this.y - this.sprite.height/2;
+    ctx.drawImage(this.sprite, x, y);
+  }
+}
+
+Entity.prototype.outsideScreen = function() {
+  return (this.x > this.game.surfaceWidth || this.x < 0 ||
+    this.y > this.game.surfaceHeight || this.y < 0);
+}
+
 
 
 var canvas = document.getElementById('surface');
